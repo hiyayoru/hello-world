2d85ee5cf0c47a8d7ee98d9e15efbd0096d3c776
Balake <55868200+hiyayoru@users.noreply.github.com>
2024-03-17 21:59:16 -0500

    Delete Project5-Masters_1.py

--- Project5-Masters_1.py
+++
@@ -1,6 +0,0 @@
-base = int(input("Enter the base of the triangle: "))
-height = int(input("Enter the height of the triangle: "))
-
-area = .5 * base * height
-
-print("The area of the triangle is", area, "square units.")
